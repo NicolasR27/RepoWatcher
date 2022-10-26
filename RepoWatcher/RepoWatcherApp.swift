a7afefaf25dc5c85d17c9e31a77dda68ecb8a3e7
Nicolas Rios <Nickrios33@yahoo.com>
2022-10-25 23:56:14 -0700

    Initial Commit

---
+++ RepoWatcher/RepoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoWatcherApp.swift
+//  RepoWatcher
+//
+//  Created by Nicolas Rios on 10/25/22.
+//
+
+import SwiftUI
+
+@main
+struct RepoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
